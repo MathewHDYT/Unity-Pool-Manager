1964bd8b517023ce49641e897f574095d67328f6
MathewHDYT <48954742+MathewHDYT@users.noreply.github.com>
2021-09-06 17:41:41 +0200

    Add new functionalities
    Add possibility to change pool size of an already created pool and the possibility to make a pool dynamic so that it will increase its size dynamically if there isn't a instance out of the pool that is invisible to the camera.

--- Example Project/Assets/Scripts/PoolManager.cs
+++ Example Project/Assets/Scripts/PoolManager.cs
@@ -21,26 +21,35 @@ public class PoolManager : MonoBehaviour {
     #endregion
 
     private Dictionary<int, Queue<ObjectInstance>> poolDictionary = new Dictionary<int, Queue<ObjectInstance>>();
+    private Dictionary<int, bool> dynamicPoolingDictionary = new Dictionary<int, bool>();
 
+    
     /// <summary>
     /// Instantiates and disables the given amount of Prefabs and adds them into the poolDictionary.
     /// </summary>
     /// <param name="prefab">Prefab we want to save into our pool.</param>
     /// <param name="poolSize">Amount of instances of the prefab we want to save into our pool.</param>
-    public void CreatePool(GameObject prefab, int poolSize) {
-        // Get InstanceID of the given GameObject.
+    /// <param name="dynamicPooling">Defines if the poolSize should increase dynamically, if there are only visible instances of the given prefab to use.</param>
+    public void CreatePool(GameObject prefab, int poolSize, bool dynamicPooling = false) {
+        // Get InstanceID of the given gameObject.
         int poolKey = prefab.GetInstanceID();
 
-        // Check if the poolDictionary doesn't already contains our Prefab.
+        // Check if the poolDictionary doesn't already contain our prefab pool.
         if (!poolDictionary.ContainsKey(poolKey)) {
             // Add the InstanceID to the poolDictionary.
             poolDictionary.Add(poolKey, new Queue<ObjectInstance>());
 
-            // Enqueue disabled GameObjects into the Queue equal to the poolSize.
-            for (int i = 0; i < poolSize; ++i) {
+            // Enqueue disabled gameObjects into the Queue equal to the poolSize.
+            for (; poolSize > 0; --i) {
                 ObjectInstance newObject = new ObjectInstance(Instantiate(prefab) as GameObject);
                 poolDictionary[poolKey].Enqueue(newObject);
             }
+        }
+        
+        // Check if the expandablePoolDictionary doesn't already contain our expandable bool.
+        if (!expandablePoolDictionary.ContainsKey(poolkey)) {
+            // Add the InstanceID to the expandablePoolDictionary.
+            expandablePoolDictionary.Add(poolKey, dynamicPooling);
         }
     }
 
@@ -50,10 +59,10 @@ public class PoolManager : MonoBehaviour {
     /// <param name="prefab">Pool we want to change the default parent at.</param>
     /// <param name="parent">Transform we want to parent our pool too.</param>
     public void ParentPool(GameObject prefab, Transform parent) {
-        // Get InstanceID of the given GameObject.
+        // Get InstanceID of the given gameObject.
         int poolKey = prefab.GetInstanceID();
 
-        // Create new GameObject to store the pooled GameObjects.
+        // Create new gameObject to store the pooled gameObjects.
         GameObject poolHolder = new GameObject(prefab.name + " Pool");
         poolHolder.transform.SetParent(parent);
 
@@ -66,6 +75,41 @@ public class PoolManager : MonoBehaviour {
             }
         }
     }
+    
+    /// <summary>
+    /// Instantiates and disables the given amount of Prefabs and adds them into the poolDictionary into the already existing pool.
+    /// </summary>
+    /// <param name="prefab">Prefab we want to save into our pool.</param>
+    /// <param name="poolSize">Additional amount of instances of the prefab we want to save into our pool.</param>
+    public void IncreasePoolSize(GameObject prefab, int difference) {
+        // Get InstanceID of the given gameObject.
+        int poolKey = prefab.GetInstanceID();
+
+        // Check if the poolDictionary already contains our prefab pool.
+        if (poolDictionary.ContainsKey(poolKey)) {
+            // Enqueue disabled gameObjects into the Queue equal to the poolSize.
+            for (; difference > 0; --i) {
+                ObjectInstance newObject = new ObjectInstance(Instantiate(prefab) as GameObject);
+                poolDictionary[poolKey].Enqueue(newObject);
+            }
+        }
+    }
+    
+    /// <summary>
+    /// En -or disables dynamic pooling on the pool with the instances of the given prefab.
+    /// </summary>
+    /// <param name="prefab">Prefab we want to en -or disable dynamic pooling on the pool off.</param>
+    /// <param name="dynamicPooling">Defines if the poolSize should increase dynamically, if there are only visible instances of the given prefab to use.</param>
+    public void EnableDynamicPooling(GameObject prefab, bool dynamicPooling = true) {
+        // Get InstanceID of the given gameObject.
+        int poolKey = prefab.GetInstanceID();
+        
+        // Check if the expandablePoolDictionary already contains our expandable bool.
+        if (expandablePoolDictionary.ContainsKey(poolkey)) {
+            // Adjust the value of to the expandablePoolDictionar at the given InstanceID.
+            expandablePoolDictionary[poolKey] = dynamicPooling;
+        }
+    }
 
     /// <summary>
     /// Takes the given prefab and gets the first deactivated instance to reuse.
@@ -75,7 +119,7 @@ public class PoolManager : MonoBehaviour {
     /// <param name="rotation">Rotation we want to set for the reused object.</param>
     /// <param name="velocity">Velocity we want to set for the reused object.</param>
     public void ReuseObject(GameObject prefab, Vector3 position = Vector3.zero, Quaternion rotation = Quaternion.identity, Vector2 velocity = Vector2.zero) {
-        // Get InstanceID of the given GameObject.
+        // Get InstanceID of the given gameObject.
         int poolKey = prefab.GetInstanceID();
 
         // Check if the poolDictionary already contains our Prefab.
@@ -83,13 +127,47 @@ public class PoolManager : MonoBehaviour {
             // First we try to reuse objects that are invisble.
             var objectToReuse = poolDictionary[poolKey].Where(x => !x.IsVisible()).FirstOrDefault();
             if (objectToReuse == null) {
-                // If there are none we just use the first one from our queue.
-                objectToReuse = poolDictionary[poolKey].Dequeue();
+                objectToReuse = UseDynamicPooledObject(poolKey);
             }
             poolDictionary[poolKey].Enqueue(objectToReuse);
             objectToReuse.Reuse(position, rotation, velocity);
         }
     }
+    
+    /// <summary>
+    /// Takes the given prefab and returns the first deactivated instance or creates a new instance to reuse.
+    /// </summary>
+    /// <param name="prefab">Prefab we want to reuse.</param>
+    /// <param name="prefab">Prefab we want to reuse.</param>
+    /// <returns>Newly created instance in our pool of the given prefab or our last used instance in case dymaicPooling is disabled.</returns>
+    private GameObject UseDynamicPooledObject(GameObject prefab) {
+        GameObject dynamicObject = null;
+        
+        // Get InstanceID of the given gameObject.
+        int poolKey = prefab.GetInstanceID();
+        
+        // Check if the expandablePoolDictionary already contains our expandable bool
+        // and copy its value into bool dynamicPooling.
+        if (expandablePoolDictionary.TryGetValue(poolkey, out bool dynamicPooling)) {
+            if (dynamicPooling) {
+                // Increase poolSize by the needed amount of new instances.
+                IncreasePoolSize(prefab, 1);
+                // Return the newly create instance.
+                dynamicObject = poolDictionary[poolKey].Where(x => !x.IsVisible()).FirstOrDefault();
+            }
+        }
+        
+        // Check if we got a newly instantiated object from our prefab pool.
+        if (dynamicObject == null) {
+            // If not return the last used instance from the prefab pool.
+            dynamicObject = poolDictionary[poolKey].Dequeue();
+        }
+        
+        return dynamicObject
+    }
+    
+                // If there are none we just use the first one from our queue.
+                objectToReuse = poolDictionary[poolKey].Dequeue();
 
     /// <summary>
     /// Handles components needed for managing of the pooled gameobjects.
